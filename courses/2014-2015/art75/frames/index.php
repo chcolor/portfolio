fce29e94794ebd11d7de860cb600ac497d1d426b
chcolor <christine.d.huffman@gmail.com>
2015-04-01 18:40:49 -0700

    4/1

---
+++ courses/2014-2015/art75/frames/index.php
@@ -0,0 +1,37 @@
+<?php require('../../../../common/main.php') ?>
+<!doctype html>
+
+<html lang="en">
+	<head>
+    	<?php import('head-meta.php') ?>
+    
+</head>
+
+<body>
+
+<header>
+	<?php import('body-header.php') ?>
+    
+</header>
+<article>
+ <div class="wrapper"> 
+ <div class="current">  
+ <h1> Frames </h1>
+ 
+ <video class="center" height="450" controls>
+  <source src="video/frames.mp4" type="video/mp4">
+Your browser does not support the video tag.
+</video>
+
+<div>
+
+
+<p class="txtblk center">This video is compiled from 30 individual videos that I shot around the San Jose area. The prompt for the project was to create a 30 second video comprised of clips focused on framing. Each video was supposed to transition to the next in an intentional way. I decided to take a principle of photography and see how it translated into video. The result is an interesting mashup of ambient sound and diverse locations.</p></div>
+</div> <!-- close wrapper -->
+</article>
+
+
+<?php import('body-footer.php') ?>
+</body>
+    
+</html>
---
+++ courses/2014-2015/art75/nonnarrative/index.php
@@ -0,0 +1,37 @@
+<?php require('../../../../common/main.php') ?>
+<!doctype html>
+
+<html lang="en">
+	<head>
+    	<?php import('head-meta.php') ?>
+    
+</head>
+
+<body>
+
+<header>
+	<?php import('body-header.php') ?>
+    
+</header>
+<article>
+ <div class="wrapper"> 
+ <div class="current">  
+ <h1> Working outside of Narrative </h1>
+ 
+ <video class="center" height="450" controls>
+  <source src="video/nonnarrative.mp4" type="video/mp4">
+Your browser does not support the video tag.
+</video>
+
+<div>
+
+
+<p class="txtblk center">The prompt for this exercise was to take video that we shot, combine it with "found" audio and video and create a work that did not conform to traditional narrative standards. I used video that I shot at Alviso Marina County Park and combined it with videos found on the internet archive at <a href="www.archive.org" target="_blank">Archive.org.</a></p></div>
+</div> <!-- close wrapper -->
+</article>
+
+
+<?php import('body-footer.php') ?>
+</body>
+    
+</html>
